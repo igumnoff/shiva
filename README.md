58c9fef72d1fb6cec9a760aa739dd614e0f84bf7
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-03-25 15:39:48 +0500

    cli

--- README.md
+++ README.md
@@ -63,7 +63,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ### Install Rust for Windows
 ```bash
 https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe
-gti clone https://github.com/igumnoff/shiva.git
 ```
 ### Build executable Shiva
 ```bash
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,29 @@
-use shiva::core::*;
+use clap::Parser;
 
+
+#[derive(Parser, Debug)]
+#[command(name="shiva", author, version, about, long_about = None)]
+struct Args {
+
+    #[arg(long)]
+    input_file: Option<String>,
+
+    #[arg(long)]
+    output_file: Option<String>,
+
+    #[arg(long)]
+    input_format: String,
+
+    #[arg(long)]
+    output_format: String,
+
+
+
+}
 fn main() {
+
+
+    let args = Args::parse();
+
 
 }
