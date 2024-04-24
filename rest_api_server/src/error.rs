a5e2826559e4cd0c053148e05d3b839a74b51686
Lucky-49 <lonely1001z@gmail.com>
2024-04-24 21:29:41 +1100

    create client error

--- rest_api_server/src/error.rs
+++ rest_api_server/src/error.rs
@@ -1,9 +1,10 @@
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
+use strum_macros::AsRefStr;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, AsRefStr)]
 pub enum Error {
     LoginFail,
 
@@ -28,4 +29,38 @@ impl IntoResponse for Error {
         response
 
     }
+}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            //Ошибки auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => {
+                (StatusCode::FORBIDDEN, ClientError::NO_AUTH)
+            }
+            //Ошибки модели
+            Self::TicketDeleteFailNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            //Ошибки клиента
+            _ => (StatusCode::INTERNAL_SERVER_ERROR,
+                  ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, AsRefStr)]
+#[allow(non_camel_case_types)] //отключает ошибку правильности написания значений
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
 }
--- rest_api_server/src/main.rs
+++ rest_api_server/src/main.rs
@@ -4,11 +4,13 @@ use crate::model::ModelController;
 use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
-use axum::{middleware, Router};
+use axum::{Json, middleware, Router};
 use serde::Deserialize;
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
+use uuid::Uuid;
 
 mod ctx; //модуль контекста
 mod error;
@@ -51,8 +53,31 @@ async fn main() -> Result<()> {
 async fn main_response_mapper(res: Response) -> Response {
     println!("-->> {:<12} - main_response_mapper", "RES_MAPPER");
 
+    let uuid = Uuid:: new_v4();
+
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // Если есть ошибка клиента, то создаем новый ответ
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+            println!("      ->> client_error_body: {client_error_body}");
+
+            //создаём отчет для client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    println!("    ->> server log line - {uuid} - Error: {service_error:?}");
+
     println!();
-    res
+    error_response.unwrap_or(res)
 }
 
 fn routes_static() -> Router {
