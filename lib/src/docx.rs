6e02119b5011ba10061c8c52023ffa2a473ef8a7
longlom <long.lom@yandex.ru>
2024-05-11 22:15:57 +0300

    SHIVA-20: add docx-rs

--- lib/src/docx.rs
+++ lib/src/docx.rs
@@ -5,273 +5,123 @@ use crate::core::{
 };
 
 use bytes::{Buf, Bytes};
-use quick_xml::events::BytesEnd;
-use quick_xml::name::QName;
-use quick_xml::{
-    events::{BytesStart, Event},
-    Reader,
-};
+use docx_rs::{read_docx, ParagraphStyle, RunChild, TableRowChild};
 use std::borrow::Cow;
 use std::{
     collections::HashMap,
     io::{BufReader, Cursor, Read, Seek, SeekFrom},
 };
-use zip::ZipArchive;
 
-enum ParagraphStyle {
-    Heading1,
-    Heading2,
-    Normal,
-    BodyText,
-}
-#[derive(Debug)]
-struct ElementBuilder {
-    element: Option<Element>,
-    table_grid_count: usize,
-}
-
-impl ElementBuilder {
-    fn new(tag_name: QName) -> Self {
-        println!("tag_name - {:?}", tag_name);
-        match tag_name {
-            QName(b"w:p") => {
-                return Self {
-                    element: Some(Paragraph { elements: vec![] }),
-                    table_grid_count: 0,
-                };
-            }
-            QName(b"w:tbl") => {
-                return Self {
-                    element: Some(Table {
-                        headers: vec![],
-                        rows: vec![],
-                    }),
-                    table_grid_count: 0,
-                };
-            }
-            _ => {
-                return Self {
-                    table_grid_count: 0,
-                    element: None,
-                };
-            }
-        }
-    }
-
-    fn add_empty_tag(&mut self, e: BytesStart) {
-        match e.name().0 {
-            b"w:pStyle" => {
-                let attrs = e
-                    .attributes()
-                    .find(|e| {
-                        let a = e.as_ref().unwrap();
-                        return a.key == QName(b"w:val");
-                    })
-                    .unwrap()
-                    .unwrap();
-
-                let el = self.element.as_mut();
-
-                match el {
-                    Some(Element::Paragraph { elements }) => {
-                        let value = attrs.value.into_owned();
-
-                        match &value[..] {
-                            b"Heading1" => elements.push(Header {
-                                level: 1,
-                                text: "".to_string(),
-                            }),
-                            b"Heading2" => elements.push(Header {
-                                level: 2,
-                                text: "".to_string(),
-                            }),
-                            b"Normal" | b"BodyText" => elements.push(Text {
-                                text: "".to_string(),
-                                size: 16,
-                            }),
-                            _ => {}
-                        }
-                    }
-
-                    Some(Element::Table { headers, rows }) => {
-                        let value = attrs.value.into_owned();
-                        println!("value - {:?}", String::from_utf8_lossy(&value[..]));
-                    }
-
-                    None => {}
-                    _ => {}
-                }
-            }
-            b"w:gridCol" => {
-                self.table_grid_count += 1;
-            }
-            _ => {}
-        }
-    }
-
-    fn add_text(&mut self, txt: String) {
-        println!("mut self - {:?}", self.element);
-
-        let el = self.element.as_mut();
-        println!("add_text txt - {:?}", txt);
-        match el {
-            Some(Element::Paragraph { elements }) => {
-                let mut last_el = elements.pop();
-                let last_el = last_el.as_mut();
-
-                match last_el {
-                    Some(Element::Text { text, size }) => {
-                        text.push_str(&txt);
-                        elements.push(last_el.unwrap().clone());
-                    }
-                    Some(Element::Header { level, text }) => {
-                        text.push_str(&txt);
-                        elements.push(last_el.unwrap().clone());
-                    }
-                    _ => {}
-                }
-            }
-            Some(Element::Table { headers, rows }) => {
-                if headers.len() < self.table_grid_count {
-                    headers.push(TableHeader {
-                        element: Element::Text {
-                            text: txt,
-                            size: 16,
-                        },
-                        width: 30.,
-                    })
-                } else {
-                    let last_index;
-                    if rows.len() == 0 {
-                        last_index = 0;
-                    } else {
-                        last_index = rows.len() - 1;
-                    }
-                    match rows.get_mut(last_index) {
-                        Some(TableRow { cells }) => {
-                            if cells.len() < self.table_grid_count {
-                                cells.push(TableCell {
-                                    element: Element::Text {
-                                        text: txt,
-                                        size: 16,
-                                    },
-                                })
-                            }
-                        }
-                        None => rows.push(TableRow {
-                            cells: vec![TableCell {
-                                element: Element::Text {
-                                    text: txt,
-                                    size: 16,
-                                },
-                            }],
-                        }),
-                    }
-                }
-            }
-            None => {}
-            _ => {}
-        }
-    }
-
-    fn finish_element(&mut self, e: BytesEnd) -> bool {
-        let el = self.element.as_mut();
-        match el {
-            Some(Element::Paragraph { elements }) => {
-                if e.name() == QName(b"w:p") {
-                    return true;
-                }
-            }
-            Some(Element::Table { headers, rows }) => {
-                if e.name() == QName(b"w:tbl") {
-                    return true;
-                }
-            }
-            None => {}
-            _ => {}
-        }
-        return false;
-    }
-}
-
-impl Default for ElementBuilder {
-    fn default() -> Self {
-        Self {
-            element: None,
-            table_grid_count: 0,
-        }
-    }
-}
 pub struct Transformer;
 
 impl TransformerTrait for Transformer {
     fn parse(document: &Bytes, _images: &HashMap<String, Bytes>) -> anyhow::Result<Document> {
-        fn parse_docx_xml(content: Vec<u8>) {
-            let mut xml = Reader::from_reader(content.reader());
+        fn extract_text(doc_element: &docx_rs::Paragraph) -> String {
+            for c in &doc_element.children {
+                match c {
+                    docx_rs::ParagraphChild::Run(run) => {
+                        println!("run.children - {:?} - ", run.children);
+                        if run.children.len() == 0 {
+                            return "".to_string();
+                        }
+                        if let RunChild::Text(t) = &run.children[0] {
+                            return t.text.to_string();
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            return "".to_string();
+        }
 
-            let mut buf = Vec::new();
+        let mut docx = read_docx(&document)?;
+        // println!("docx.document.children - {:?}", docx.document.children);
+        const HEADING1: &str = "Heading1";
+        const HEADING2: &str = "Heading2";
+        const NORMAL: &str = "Normal";
+        const BODY_TEXT: &str = "BodyText";
+        // const HEADING1: &str = "Heading1";
+        let mut result: Vec<Element> = vec![];
+        for ch in docx.document.children {
+            println!("ch - {:?}", ch);
 
-            let mut element: ElementBuilder = ElementBuilder::default();
-            let mut result: Vec<Element> = vec![];
-            loop {
-                let read_data = xml.read_event_into(&mut buf);
+            match ch {
+                docx_rs::DocumentChild::Paragraph(par) => {
+                    match &par.property.style {
+                        Some(ParagraphStyle { val }) => match val.as_str() {
+                            HEADING1 => {
+                                let text = extract_text(&par);
+                                let element = Element::Header {
+                                    level: 1,
+                                    text: text,
+                                };
 
-                match read_data {
-                    Ok(Event::Start(e)) => {
-                        println!("Event::Start: {:?}", e);
-                        match e.name() {
-                            QName(b"w:p") => {
-                                if element.element.is_none() {
-                                    element = ElementBuilder::new(e.name());
-                                }
+                                result.push(element);
                             }
-                            QName(b"w:tbl") => {
-                                element = ElementBuilder::new(e.name());
-                                println!("element - :tbl{:?}", element);
+                            HEADING2 => {
+                                let text = extract_text(&par);
+                                let element = Element::Header {
+                                    level: 2,
+                                    text: text,
+                                };
+
+                                result.push(element);
+                            }
+
+                            BODY_TEXT => {
+                                let text = extract_text(&par);
+                                let element = Element::Text { text, size: 16 };
+
+                                result.push(element);
+                            }
+
+                            NORMAL => {
+                                let text = extract_text(&par);
+                                let element = Element::Text { text, size: 16 };
+
+                                result.push(element);
                             }
 
                             _ => {}
-                        }
+                        },
+                        _ => {}
                     }
-                    Ok(Event::Empty(e)) => element.add_empty_tag(e),
-
-                    Ok(Event::Text(e)) => {
-                        println!("Event::Text(e) - {:?}", e);
-                        element.add_text(e.unescape().unwrap().into_owned());
-                    }
-                    Ok(Event::End(e)) => {
-                        if element.finish_element(e) {
-                            println!("element End - {:?}", element);
-                            result.push(element.element.take().unwrap());
-                        }
-                    }
-
-                    Ok(Event::Eof) => break,
-
-                    _ => {}
                 }
+                docx_rs::DocumentChild::Table(table) => {
+                    let mut rows = vec![];
+                    for row in &table.rows {
+                        let docx_rs::TableChild::TableRow(tr) = row;
+                        // println!("tr - {:?}", tr);
+                        let mut cells = TableRow { cells: vec![] };
 
-                buf.clear();
+                        for table_cell in &tr.cells {
+                            let TableRowChild::TableCell(tc) = table_cell;
+                            println!("tc - {:?}", tc);
+                            for ch in &tc.children {
+                                match ch {
+                                    docx_rs::TableCellContent::Paragraph(par) => {
+                                        let text = extract_text(&par);
+                                        cells.cells.push(TableCell {
+                                            element: Element::Text { text, size: 16 },
+                                        });
+                                    }
+                                    _ => {}
+                                }
+                            }
+                        }
+                        rows.push(cells);
+                    }
+
+                    result.push(Table {
+                        headers: vec![],
+                        rows,
+                    });
+                }
+                _ => {}
             }
-
-            println!("result - {:?}", result);
         }
 
-        let cursor = Cursor::new(document);
-        let mut zip = ZipArchive::new(cursor).expect("Wasn't able to read document");
-        let mut main_document = zip
-            .by_name("word/document.xml")
-            .expect("Didn't find document xml file in docx");
-
-        let mut content = vec![];
-
-        main_document
-            .read_to_end(&mut content)
-            .expect("Couldn't read content of document into buffer");
-
-        parse_docx_xml(content);
-
-        todo!()
+        Ok(Document::new(result))
     }
     fn generate(_document: &Document) -> anyhow::Result<(Bytes, HashMap<String, Bytes>)> {
         todo!()
@@ -293,6 +143,11 @@ mod tests {
         let images = HashMap::new();
         let parsed = docx::Transformer::parse(&bytes, &images);
         assert!(parsed.is_ok());
+        println!(
+            "--------------------------------------------\n parsed - {:?}",
+            parsed
+        );
+
         Ok(())
     }
 }
