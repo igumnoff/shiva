e4d6a913c2d95c04e954ed32f17295e37f3eb47f
longlom <long.lom@yandex.ru>
2024-05-11 18:37:15 +0300

    SHICA-20: test

--- lib/src/docx.rs
+++ lib/src/docx.rs
@@ -1,25 +1,206 @@
 use crate::core::Element::{Header, Hyperlink, Image, List, Paragraph, Table, Text};
 use crate::core::{
-    Document, Element, ImageType, ListItem, ParserError, TableHeader, TableRow, TransformerTrait,
+    Document, Element, ImageType, ListItem, ParserError, TableCell, TableHeader, TableRow,
+    TransformerTrait,
 };
 
 use bytes::{Buf, Bytes};
+use quick_xml::events::BytesEnd;
 use quick_xml::name::QName;
 use quick_xml::{
     events::{BytesStart, Event},
     Reader,
 };
+use std::borrow::Cow;
 use std::{
     collections::HashMap,
     io::{BufReader, Cursor, Read, Seek, SeekFrom},
 };
 use zip::ZipArchive;
 
-pub enum ParagraphStyle {
+enum ParagraphStyle {
     Heading1,
     Heading2,
     Normal,
     BodyText,
+}
+#[derive(Debug)]
+struct ElementBuilder {
+    element: Option<Element>,
+    table_grid_count: usize,
+}
+
+impl ElementBuilder {
+    fn new(tag_name: QName) -> Self {
+        println!("tag_name - {:?}", tag_name);
+        match tag_name {
+            QName(b"w:p") => {
+                return Self {
+                    element: Some(Paragraph { elements: vec![] }),
+                    table_grid_count: 0,
+                };
+            }
+            QName(b"w:tbl") => {
+                return Self {
+                    element: Some(Table {
+                        headers: vec![],
+                        rows: vec![],
+                    }),
+                    table_grid_count: 0,
+                };
+            }
+            _ => {
+                return Self {
+                    table_grid_count: 0,
+                    element: None,
+                };
+            }
+        }
+    }
+
+    fn add_empty_tag(&mut self, e: BytesStart) {
+        match e.name().0 {
+            b"w:pStyle" => {
+                let attrs = e
+                    .attributes()
+                    .find(|e| {
+                        let a = e.as_ref().unwrap();
+                        return a.key == QName(b"w:val");
+                    })
+                    .unwrap()
+                    .unwrap();
+
+                let el = self.element.as_mut();
+
+                match el {
+                    Some(Element::Paragraph { elements }) => {
+                        let value = attrs.value.into_owned();
+
+                        match &value[..] {
+                            b"Heading1" => elements.push(Header {
+                                level: 1,
+                                text: "".to_string(),
+                            }),
+                            b"Heading2" => elements.push(Header {
+                                level: 2,
+                                text: "".to_string(),
+                            }),
+                            b"Normal" | b"BodyText" => elements.push(Text {
+                                text: "".to_string(),
+                                size: 16,
+                            }),
+                            _ => {}
+                        }
+                    }
+
+                    Some(Element::Table { headers, rows }) => {
+                        let value = attrs.value.into_owned();
+                        println!("value - {:?}", String::from_utf8_lossy(&value[..]));
+                    }
+
+                    None => {}
+                    _ => {}
+                }
+            }
+            b"w:gridCol" => {
+                self.table_grid_count += 1;
+            }
+            _ => {}
+        }
+    }
+
+    fn add_text(&mut self, txt: String) {
+        println!("mut self - {:?}", self.element);
+
+        let el = self.element.as_mut();
+        println!("add_text txt - {:?}", txt);
+        match el {
+            Some(Element::Paragraph { elements }) => {
+                let mut last_el = elements.pop();
+                let last_el = last_el.as_mut();
+
+                match last_el {
+                    Some(Element::Text { text, size }) => {
+                        text.push_str(&txt);
+                        elements.push(last_el.unwrap().clone());
+                    }
+                    Some(Element::Header { level, text }) => {
+                        text.push_str(&txt);
+                        elements.push(last_el.unwrap().clone());
+                    }
+                    _ => {}
+                }
+            }
+            Some(Element::Table { headers, rows }) => {
+                if headers.len() < self.table_grid_count {
+                    headers.push(TableHeader {
+                        element: Element::Text {
+                            text: txt,
+                            size: 16,
+                        },
+                        width: 30.,
+                    })
+                } else {
+                    let last_index;
+                    if rows.len() == 0 {
+                        last_index = 0;
+                    } else {
+                        last_index = rows.len() - 1;
+                    }
+                    match rows.get_mut(last_index) {
+                        Some(TableRow { cells }) => {
+                            if cells.len() < self.table_grid_count {
+                                cells.push(TableCell {
+                                    element: Element::Text {
+                                        text: txt,
+                                        size: 16,
+                                    },
+                                })
+                            }
+                        }
+                        None => rows.push(TableRow {
+                            cells: vec![TableCell {
+                                element: Element::Text {
+                                    text: txt,
+                                    size: 16,
+                                },
+                            }],
+                        }),
+                    }
+                }
+            }
+            None => {}
+            _ => {}
+        }
+    }
+
+    fn finish_element(&mut self, e: BytesEnd) -> bool {
+        let el = self.element.as_mut();
+        match el {
+            Some(Element::Paragraph { elements }) => {
+                if e.name() == QName(b"w:p") {
+                    return true;
+                }
+            }
+            Some(Element::Table { headers, rows }) => {
+                if e.name() == QName(b"w:tbl") {
+                    return true;
+                }
+            }
+            None => {}
+            _ => {}
+        }
+        return false;
+    }
+}
+
+impl Default for ElementBuilder {
+    fn default() -> Self {
+        Self {
+            element: None,
+            table_grid_count: 0,
+        }
+    }
 }
 pub struct Transformer;
 
@@ -29,149 +210,42 @@ impl TransformerTrait for Transformer {
             let mut xml = Reader::from_reader(content.reader());
 
             let mut buf = Vec::new();
-            // let mut txt = Vec::new();
-            let mut count = 0;
 
+            let mut element: ElementBuilder = ElementBuilder::default();
+            let mut result: Vec<Element> = vec![];
             loop {
-                // NOTE: this is the generic case when we don't know about the input BufRead.
-                // when the input is a &str or a &[u8], we don't actually need to use another
-                // buffer, we could directly call `reader.read_event()`
                 let read_data = xml.read_event_into(&mut buf);
 
-                // println!("read_data - {:?}", read_data);
                 match read_data {
-                    // Err(e) => panic!("Error at position {}: {:?}", xml.buffer_position(), e),
-                    // exits the loop when reaching end of file
                     Ok(Event::Start(e)) => {
-                        // println!("Event::Start: {:?}", e);
-                        match e.name().0 {
-                            b"w:body" => {
-                                break;
-                            }
-                            _ => {}
-                        }
-                    }
-                    Ok(Event::Empty(e)) => {
-                        let attrs = e
-                            .attributes()
-                            .map(|e| e.unwrap())
-                            .enumerate()
-                            .collect::<Vec<_>>();
-                        // for att in attrs {
-                        //    if att.1.key == ""
-                        // }
-                        // println!("Event::Empty: {:?}", e.name().0 == b"w:pStyle");
-                        // println!("Event::Empty: {:?}", e.local_name());
-                        match e.name().0 {
-                            b"w:pStyle" => {}
-                            _ => {}
-                        }
-                        // println!("Event::Empty: {:?}", e.attributes().map(|e| e.unwrap()).enumerate().collect::<Vec<_>>())
-                    }
-                    Ok(Event::End(e)) => {
-                        println!("Event::End: {:?}", e)
-                    }
-
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-
-                    // There are several other `Event`s we do not consider here
-                    Ok(Event::Eof) => break,
-
-                    _ => {}
-                }
-
-                buf.clear();
-            }
-
-            let mut element;
-            // println!("after first");
-            let mut depth = 0;
-
-            loop {
-                // NOTE: this is the generic case when we don't know about the input BufRead.
-                // when the input is a &str or a &[u8], we don't actually need to use another
-                // buffer, we could directly call `reader.read_event()`
-                let read_data = xml.read_event_into(&mut buf);
-                // if let Ok(Event::Start(e)) = read_data{
-                //     buf.clear();
-
-                //     let end = e.to_end().into_owned();
-                //     let span = xml.read_to_end_into(end.name(), &mut buf);
-                // }
-
-                // println!("read_data - {:?}", read_data);
-                match read_data {
-                    // Err(e) => panic!("Error at position {}: {:?}", xml.buffer_position(), e),
-                    // exits the loop when reaching end of file
-                    Ok(Event::Start(e)) => {
-                        match e.name().0 {
-                            b"w:p" => {
-                                depth += 1;
-                                element = Paragraph { elements: vec![] };
-                            }
-                            _ => {}
-                        }
-                        // println!("Event::Start: {:?}", e);
-
-                        // let end = e.to_end().into_owned();
-                        // println!("end.name(): {:?}", end.name());
-
-                        // let span = xml.read_to_end_into(end.name(), &mut buf).unwrap();
-
-                        // println!("buf - {:?}", buf);
-                        // println!("span - {:?}", span);
-                    }
-                    Ok(Event::Empty(e)) => {
-                        match e.name().0 {
-                            b"w:pStyle" => {
-                                if depth != 0 {
-                                    let attrs = e
-                                        .attributes()
-                                        .find(|e| {
-                                            let a = e.as_ref().unwrap();
-                                            return a.key == QName(b"w:val");
-                                        }).unwrap().unwrap();
-
-                                    println!("Event::Empty attrs: {:?}", attrs);
-                                    println!("Event::Empty: {:?}", e.name().0 == b"w:pStyle");
-                                    println!("Event::Empty: {:?}", e.local_name());
+                        println!("Event::Start: {:?}", e);
+                        match e.name() {
+                            QName(b"w:p") => {
+                                if element.element.is_none() {
+                                    element = ElementBuilder::new(e.name());
                                 }
                             }
+                            QName(b"w:tbl") => {
+                                element = ElementBuilder::new(e.name());
+                                println!("element - :tbl{:?}", element);
+                            }
+
                             _ => {}
                         }
-                        // if depth != 0 {
-                        //     let attrs = e
-                        //         .attributes()
-                        //         .map(|e| e.unwrap())
-                        //         .enumerate()
-                        //         .collect::<Vec<_>>();
-                        //     // for att in attrs {
-                        //     //    if att.1.key == ""
-                        //     // }
-                        //     println!("Event::Empty attrs: {:?}", attrs);
-                        //     println!("Event::Empty: {:?}", e.name().0 == b"w:pStyle");
-                        //     println!("Event::Empty: {:?}", e.local_name());
-                        // match e.name().0 {
-                        //     b"w:pStyle" => {
-
-                        //     }
-                        //     _ => {}
-                        // }
-                        // }
-
-                        // println!("Event::Empty: {:?}", e.attributes().map(|e| e.unwrap()).enumerate().collect::<Vec<_>>())
                     }
-                    // Ok(Event::End(e)) => {
-                    //     println!("Event::End: {:?}", e)
-                    // }
+                    Ok(Event::Empty(e)) => element.add_empty_tag(e),
 
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-                    // Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
+                    Ok(Event::Text(e)) => {
+                        println!("Event::Text(e) - {:?}", e);
+                        element.add_text(e.unescape().unwrap().into_owned());
+                    }
+                    Ok(Event::End(e)) => {
+                        if element.finish_element(e) {
+                            println!("element End - {:?}", element);
+                            result.push(element.element.take().unwrap());
+                        }
+                    }
 
-                    // There are several other `Event`s we do not consider here
                     Ok(Event::Eof) => break,
 
                     _ => {}
@@ -179,9 +253,9 @@ impl TransformerTrait for Transformer {
 
                 buf.clear();
             }
-        }
 
-        let mut elements: Vec<Element> = Vec::new();
+            println!("result - {:?}", result);
+        }
 
         let cursor = Cursor::new(document);
         let mut zip = ZipArchive::new(cursor).expect("Wasn't able to read document");
@@ -195,42 +269,8 @@ impl TransformerTrait for Transformer {
             .read_to_end(&mut content)
             .expect("Couldn't read content of document into buffer");
 
-        // let mut xml = Reader::from_reader(content.reader());
         parse_docx_xml(content);
 
-        // loop {
-        //     // NOTE: this is the generic case when we don't know about the input BufRead.
-        //     // when the input is a &str or a &[u8], we don't actually need to use another
-        //     // buffer, we could directly call `reader.read_event()`
-        //     match xml.read_event_into(&mut buf) {
-        //         Err(e) => panic!("Error at position {}: {:?}", xml.buffer_position(), e),
-        //         // exits the loop when reaching end of file
-        //         Ok(Event::Eof) => break,
-        //         Ok(Event::Start(e)) => match e.name().as_ref() {
-        //             b"w:p" => {
-        //                 // let b = e.bytes().fold(String::new(), |mut acc, e| {
-        //                 //     let e = e.unwrap();
-
-        //                 //     let e = e.ch;
-        //                 //     acc.push_str(&e);
-        //                 //     acc
-        //                 // });
-        //                 println!("attributes values: {:?}", b)
-        //             }
-        //             b"tag2" => count += 1,
-        //             _ => {
-        //                 println!("e - {:?}", e);
-        //             }
-        //         },
-        //         Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
-
-        //         // There are several other `Event`s we do not consider here
-        //         _ => (),
-        //     }
-        //     buf.clear();
-        // }
-
-        // println!("{:?}", txt);
         todo!()
     }
     fn generate(_document: &Document) -> anyhow::Result<(Bytes, HashMap<String, Bytes>)> {
