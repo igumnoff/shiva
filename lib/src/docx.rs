d7ed320d70e32a02741ffa305d5b50dbabffbd9c
longlom <long.lom@yandex.ru>
2024-05-13 12:56:39 +0300

    SHIVA-20: list parsing left

--- lib/src/docx.rs
+++ lib/src/docx.rs
@@ -5,7 +5,7 @@ use crate::core::{
 };
 
 use bytes::{Buf, Bytes};
-use docx_rs::{read_docx, ParagraphStyle, RunChild, TableRowChild};
+use docx_rs::{read_docx, NumberingProperty, ParagraphStyle, RunChild, TableRowChild};
 use std::borrow::Cow;
 use std::{
     collections::HashMap,
@@ -34,6 +34,116 @@ impl TransformerTrait for Transformer {
             return "".to_string();
         }
 
+        fn push_list_item(
+            result: &mut Vec<Element>,
+            is_collecting_list: &mut bool,
+            last_list_indent: &mut usize,
+            par: &docx_rs::Paragraph,
+            numbering_property: &NumberingProperty,
+        ) {
+            let numbered = numbering_property
+                .id
+                .as_ref()
+                .expect("No number id in list item")
+                .id
+                == 3;
+            let list_indent = numbering_property
+                .level
+                .as_ref()
+                .expect("Expect indent level to be Some")
+                .val;
+
+            if !*is_collecting_list {
+                *is_collecting_list = true;
+
+                let text = extract_text(&par);
+                let list_item = ListItem {
+                    element: Text { text, size: 16 },
+                };
+                let element = Element::List {
+                    elements: vec![list_item],
+                    numbered,
+                };
+
+                result.push(element);
+
+                *last_list_indent = list_indent;
+
+                // else if last_list_indent < list_indent {
+                //     let depth = list_indent - last_list_indent;
+                //      let mut elements =  vec![];
+                //     for _ in 0..depth-1 {
+                //      let inner_element = Element::List { elements: vec![] , numbered };
+                //      elements.push(inner_element);
+                //     }
+
+                //  }
+            } else {
+                // let last_list_element;
+
+                // let last_index = result.len() - 1;
+                // if let Element::List { elements, numbered } = result
+                //     .get_mut(last_index)
+                //     .expect("Last list should be Some")
+                // {
+
+
+                // }
+                // if *last_list_indent == list_indent {
+                //     let text = extract_text(&par);
+                //     let list_item = ListItem {
+                //         element: Text { text, size: 16 },
+                //     };
+
+                //     let last_index = result.len() - 1;
+                //     if let Element::List { elements, numbered } = result
+                //         .get_mut(last_index)
+                //         .expect("Last list should be Some")
+                //     {
+                //         elements.push(list_item);
+                //     }
+                // } else if *last_list_indent < list_indent {
+                //     let text = extract_text(&par);
+                //     let last_index = result.len() - 1;
+                //     if let Element::List { elements, numbered } = result
+                //         .get_mut(last_index)
+                //         .expect("Last list should be Some")
+                //     {
+
+                //     }
+
+                // }
+
+                // else if *last_list_indent < list_indent {
+                //     let list_depth = list_indent - *last_list_indent;
+
+                //     let mut list_elements = Element::List {
+                //             elements: vec![],
+                //             numbered,
+                //         };
+
+                //     for _ in 0..list_depth - 1 {
+                //         let el = Element::List {
+                //             elements: vec![],
+                //             numbered,
+                //         };
+                //         elements.push(el);
+                //     }
+
+                //     let last_index = elements.len() - 1;
+                //     if let Element::List { elements, numbered } = elements
+                //         .get_mut(last_index)
+                //         .expect("Last list should be Some")
+                //     {
+                //         elements.push(list_item);
+                //     }
+
+                //     *last_list_indent = list_indent;
+
+                // }
+            }
+        }
+
         let mut docx = read_docx(&document)?;
         // println!("docx.document.children - {:?}", docx.document.children);
         const HEADING1: &str = "Heading1";
@@ -42,49 +152,64 @@ impl TransformerTrait for Transformer {
         const BODY_TEXT: &str = "BodyText";
         // const HEADING1: &str = "Heading1";
         let mut result: Vec<Element> = vec![];
+        let mut is_collecting_list = false;
+        let mut last_list_indent: usize = 0;
         for ch in docx.document.children {
             println!("ch - {:?}", ch);
 
             match ch {
                 docx_rs::DocumentChild::Paragraph(par) => {
-                    match &par.property.style {
-                        Some(ParagraphStyle { val }) => match val.as_str() {
-                            HEADING1 => {
-                                let text = extract_text(&par);
-                                let element = Element::Header {
-                                    level: 1,
-                                    text: text,
-                                };
+                    if let Some(numbering_property) = &par.property.numbering_property {
+                        push_list_item(
+                            &mut result,
+                            &mut is_collecting_list,
+                            &mut last_list_indent,
+                            &par,
+                            numbering_property,
+                        );
+                    } else {
+                        is_collecting_list = false;
+                        match &par.property.style {
+                            Some(ParagraphStyle { val }) => match val.as_str() {
+                                HEADING1 => {
+                                    let text = extract_text(&par);
+                                    let element = Element::Header {
+                                        level: 1,
+                                        text: text,
+                                    };
 
-                                result.push(element);
+                                    result.push(element);
+                                }
+                                HEADING2 => {
+                                    let text = extract_text(&par);
+                                    let element = Element::Header {
+                                        level: 2,
+                                        text: text,
+                                    };
+
+                                    result.push(element);
+                                }
+
+                                BODY_TEXT => {
+                                    let text = extract_text(&par);
+                                    let element = Element::Text { text, size: 16 };
+
+                                    result.push(element);
+                                }
+
+                                NORMAL => {
+                                    let text = extract_text(&par);
+                                    let element = Element::Text { text, size: 16 };
+
+                                    result.push(element);
+                                }
+
+                                _ => {}
+                            },
+                            _ => {
+                                unimplemented!("Should implement");
                             }
-                            HEADING2 => {
-                                let text = extract_text(&par);
-                                let element = Element::Header {
-                                    level: 2,
-                                    text: text,
-                                };
-
-                                result.push(element);
-                            }
-
-                            BODY_TEXT => {
-                                let text = extract_text(&par);
-                                let element = Element::Text { text, size: 16 };
-
-                                result.push(element);
-                            }
-
-                            NORMAL => {
-                                let text = extract_text(&par);
-                                let element = Element::Text { text, size: 16 };
-
-                                result.push(element);
-                            }
-
-                            _ => {}
-                        },
-                        _ => {}
+                        }
                     }
                 }
                 docx_rs::DocumentChild::Table(table) => {
